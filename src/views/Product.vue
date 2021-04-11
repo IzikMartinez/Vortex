070c081b53784a49cf201a04804c0c7476e6eb1a
trevor <tsontag@islander.tamucc.edu>
2021-04-10 22:04:50 -0500

    Tried to copy home page over to the product page but it's having a display issue

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,13 +1,37 @@
 <template>
   <h1>hi</h1>
+    <div class="bubbles">
+      <Bubble class="bubble" title="Lorem Ipsum"></Bubble>
+      <Bubble class="bubble" title="Dolor Sit Amet"></Bubble>
+      <Bubble class="bubble" title="Consectectur"></Bubble>
+      <Bubble class="bubble" title="Adipisicing "></Bubble>
+      <Bubble class="bubble" title="Facere, Iusto"></Bubble>
+      <Bubble class="bubble" title="Mario Facto"></Bubble>
+    </div>
 </template>
 
-<script>
+<script lang="ts">
+// @ is an alias to /src
+import ProductBubble from "@/components/fragments/ProductBubble.vue";
+
 export default {
-name: "Product"
+  name: "Product",
+  components: {
+    ProductBubble
+  }
 }
 </script>
 
-<style scoped>
+<style>
+  .bubbles {
+    padding-top: 15vh;
+    padding-bottom: 5vh;
+    display: grid;
+    grid-template-columns: auto auto auto;
+  }
 
+  .bubble {
+    position: relative;
+    margin-bottom: 15px;
+  }
 </style>
